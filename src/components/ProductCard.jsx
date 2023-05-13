faef4813fce341c1dd743bfd06899ba04cf478b2
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2023-05-13 22:56:24 +0530

    created the product card for desktop devices

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,33 @@
+import { ArrowPathRoundedSquareIcon, ArrowsPointingOutIcon, HeartIcon, ShoppingCartIcon } from '@heroicons/react/24/outline'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const ProductCard = ({item}) => {
+ 
   return (
     <>
     {/* <Link to="/"> */}
-        <div className="card w-[400px] h-[500px] text-white relative">
+        <div className="card w-[400px] h-[500px] text-white relative group ">
             <img src={item.img} alt="" className='w-full h-full object-cover'/>
             <span className='absolute top-0 '>{item.desc}</span>
             <span>{item.title}</span>
+
+            <ul className='absolute top-4 right-3 text-black space-y-4 '>
+          <li className='bg-white p-2 rounded-full hover:bg-red-600 duration-300 ease-in-out hover:text-white invisible group-hover:visible'><HeartIcon className='w-6 h-6'/></li>
+
+          <li className='bg-white p-2 rounded-full hover:bg-red-600 duration-300 ease-in-out hover:text-white invisible group-hover:visible'><ArrowsPointingOutIcon className='w-6 h-6'/></li>
+
+          <li className='bg-white p-2 rounded-full hover:bg-red-600 duration-300 ease-in-out hover:text-white invisible group-hover:visible'><ShoppingCartIcon className='w-6 h-6'/></li>
+
+          
+          <li className='bg-white p-2 rounded-full hover:bg-red-600 duration-300 ease-in-out hover:text-white invisible group-hover:visible '><ArrowPathRoundedSquareIcon className='w-6 h-6'/></li>
+            </ul>
+
+            <div className='mx-4 bg-white text-black absolute bottom-0 rounded-t-lg inset-x-0 p-4'>
+              <h5>{item.desc}</h5>
+              <h6>{300}</h6>
+              <span>3 reviews</span>
+            </div>
         </div>
     {/* </Link> */}
     </>
--- src/components/ProductSlider.jsx
+++ src/components/ProductSlider.jsx
@@ -3,9 +3,14 @@ import Slider from 'infinite-react-carousel';
 import {cards} from "../dummyData.js"
 import ProductCard from './ProductCard.jsx';
 const ProductSlider = () => {
+
   return (
     <>
      <div className="container mx-auto">
+      <div className='py-10  flex justify-between items-center'>
+        <h3 className='text-3xl font-bold mr-8'>Featured Products</h3>
+        <p className='text-gray-500 w-1/2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi velit voluptas qui commodi corrupti obcaecati aperiam perferendis pariatur, natus porro, optio dignissimos reprehenderit omnis praesentium dolore id? Numquam, sapiente reiciendis!</p>
+      </div>
      <Slider dots slidesToShow={3} arrowsScroll={3} dotsScroll={3} >
    {cards.map((card)=>(
       <ProductCard item={card} key={card.id}/>
