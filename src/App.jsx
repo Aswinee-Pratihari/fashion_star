c100d60abb0c8f184c42791c5d664941a53a28aa
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2023-05-13 23:52:14 +0530

    refactor router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
-import Home from './pages/Home'
+
 
 import {
   createBrowserRouter,
@@ -72,12 +72,7 @@ function App() {
 
   return (
   <>
-<<<<<<< HEAD
  <RouterProvider router={router} />
-=======
-  <Navbar/>
-  <Home/>
->>>>>>> making_ProductCard
   </>
   )
 }
