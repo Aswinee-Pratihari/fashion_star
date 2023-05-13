26fcd78d9a8568d1ef08bfe9afb8da0fc75b3009
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2023-05-13 18:04:01 +0530

    feat:
    Did the intial routing of the site

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,74 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
 
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Home from './pages/Home';
+import Footer from './components/Footer';
 function App() {
-  const [count, setCount] = useState(0)
+  const AppLayout=()=>{
+    return(
+      <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+      </>
+    )
+  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout/> ,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        },
+        {
+          path:"/products",
+          element:<Home/>
+        },
+        {
+          path:"/products/:catId",
+          element:<Home/>
+        },
+        {
+          path:"/myProducts",
+          element:<Home/>
+        },
+        {
+          path:"/register",
+          element:<Home/>
+        },
+        {
+          path:"/login",
+          element:<Home/>
+        },
+        {
+          path:"/products",
+          element:<Home/>
+        },  {
+          path: "/chats",
+          element: <Home />,
+        },
+        {
+          path: "/chat/:id",
+          element: <Home />,
+        },
+        {
+          path: "/addProduct",
+          element: <Home />,
+        },
+      ]
+    },
+  ]);
+
   return (
   <>
-  <Navbar/>
+ <RouterProvider router={router} />
   </>
   )
 }
