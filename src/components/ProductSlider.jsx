4b47d016b0daee972af0e3e53bdc2ad287eae027
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2023-05-13 23:02:41 +0530

    refactor
    adding props to productSlider

--- src/components/ProductSlider.jsx
+++ src/components/ProductSlider.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import Slider from 'infinite-react-carousel';
 import {cards} from "../dummyData.js"
 import ProductCard from './ProductCard.jsx';
-const ProductSlider = () => {
+const ProductSlider = ({children,slidesToShow,arrowsScroll,dotsScroll}) => {
   return (
     <>
      <div className="container mx-auto">
-     <Slider dots slidesToShow={3} arrowsScroll={3} dotsScroll={3} >
-   {cards.map((card)=>(
-      <ProductCard item={card} key={card.id}/>
-   ))}
+     <Slider dots slidesToShow={slidesToShow} arrowsScroll={arrowsScroll} dotsScroll={dotsScroll} >
+   {children}
   </Slider>
      </div>
     </>
